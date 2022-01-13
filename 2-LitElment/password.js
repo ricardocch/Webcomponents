80bb22fab596068e4ac987162f8d3d4924c026a9
ricardocch <ricardo_azul4@yahoo.com>
2022-01-13 13:13:38 -0600

    se sube compoente password

--- 2-LitElment/password.js
+++ 2-LitElment/password.js
@@ -1,10 +1,36 @@
 import { html, render } from 'lit-html';
 
 class PasswordChecker extends HTMLElement {
-    constructor() {
-      super();
-      this.attachShadow({mode: 'open'});
-      this.password = this.getAttribute('password');
-    }
-    
+  constructor() {
+    super();
+    this.attachShadow({mode: 'open'});
+    this.password = this.getAttribute('password');
+  }
+
+  get password() { return this._password; }
+
+  set password(value) {
+    this._password = value;
+    this.setAttribute('password', value);
+    this.update();
+  }
+
+  update() {
+    render(this.template(), this.shadowRoot, {eventContext: this});
+  }
+
+  isValid(passwd) { 
+    const re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,}/;
+    return re.test(passwd);
+  }
+
+  template() {
+    return html`
+      <span>Your password is <strong>${this.isValid(this.password) ? 'valid 👍' : 'INVALID 👎'}</strong></span>
+      ${this.isValid(this.password) ? 
+        html`<div>Strength: <progress value=${this.password.length-3} max="5"</progress></div>` : ``}`;
+
+  }
 }
+
+customElements.define('password-checker', PasswordChecker);
